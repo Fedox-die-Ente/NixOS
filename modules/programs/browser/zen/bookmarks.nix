878dc3656472f6a9149650f7c5b7a43a02a4ba53
Florian <67076970+Fedox-die-Ente@users.noreply.github.com>
2025-10-10 20:43:49 +0200

    Remove Physics bookmarks and update Mail-Services
    Removed bookmarks related to Physics and updated Mail-Services bookmarks.

--- modules/programs/browser/zen/bookmarks.nix
+++ modules/programs/browser/zen/bookmarks.nix
@@ -26,31 +26,6 @@
           url = "https://chatgpt.com/";
         }
         {
-          name = "Physics";
-          bookmarks = [
-            {
-              name = "Qubits";
-              url = "https://arxiv.org/pdf/1904.06560";
-            }
-            {
-              name = "Multi-Qubits";
-              url = "https://arxiv.org/pdf/2502.16479";
-            }
-            {
-              name = "Cryogenic Modeling";
-              url = "https://arxiv.org/pdf/2502.01945";
-            }
-            {
-              name = "Nuclear Spin Engineering";
-              url = "https://arxiv.org/pdf/2502.18450";
-            }
-            {
-              name = "Mixed State Entanglement";
-              url = "https://arxiv.org/pdf/2502.18446";
-            }
-          ];
-        }
-        {
           name = "NixOS";
           bookmarks = [
             {
@@ -96,15 +71,15 @@
           ];
         }
         {
-          name = "Search Engines";
+          name = "Mail-Services";
           bookmarks = [
             {
-              name = "Startpage";
-              url = "https://www.startpage.com/do/mypage.pl?prfe=c602752472dd4a3d8286a7ce441403da08e5c4656092384ed3091a946a5a4a4c99962d0935b509f2866ff1fdeaa3c33a007d4d26e89149869f2f7d0bdfdb1b51aa7ae7f5f17ff4a233ff313d";
+              name = "Gmail";
+              url = "https://mail.google.com/mail/u/0/#inbox";
             }
             {
-              name = "SearX";
-              url = "https://searx.aicampground.com";
+              name = "Proton";
+              url = "https://mail.proton.me/u/0/inbox";
             }
           ];
         }
